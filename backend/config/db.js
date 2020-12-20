92c6ed49e1b5d7c969b0b2849c033c101351ba30
SieroSec <SieroSec@gmail.com>
2020-12-20 04:58:36 +0100

    Modeling Our Data

---
+++ backend/config/db.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose'
+
+const connectDB = async () => {
+   try {
+      const conn = await mongoose.connect(process.env.MONGO_URI, {
+         useUnifiedTopology: true,
+         useNewUrlParser: true,
+         useCreateIndex: true
+      })
+      console.log(`Mongo connected: ${conn.connection._connectionString}`.cyan.underline)
+      //console.log(conn.connections)
+   } catch (error) {
+      console.error(`Error: ${error.message}`.red.underline.bold);
+      process.exit(1)
+   }
+}
+
+export default connectDB
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
 import products from './data/products.js'
 
 dotenv.config();
+connectDB();
 
 const app = express();
 
@@ -10,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 const NODE_ENV = process.env.NODE_ENV || 'variable NODE_ENV is not defined'
 
 app.listen(PORT, () => {
-   console.log(`App listening in ${NODE_ENV} mode on port ${PORT}!`);
+   console.log(`App listening in ${NODE_ENV} mode on port ${PORT}!`.yellow.bold);
 });
 
 app.get('/', (req, res) => {
