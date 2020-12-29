7f4c5f6f10f7518979f5d6e307ce1221d76de263
SieroSec <SieroSec@gmail.com>
2020-12-29 13:43:42 +0100

    Section 6: Cart Reducer & Add To Cart Action

---
+++ frontend/src/screens/CartScreen.js
@@ -0,0 +1,20 @@
+// import React, { useEffect } from 'react'
+// import { useDispatch, useSelector } from 'react-redux'
+// import { Row, Col } from 'react-bootstrap'
+// import Product from '../components/Product'
+// import Message from '../components/Message'
+// import Loader from '../components/Loader'
+// import { listProducts, listsProducts } from '../actions/productActions'
+
+import React from 'react'
+
+const CartScreen = () => {
+   return (
+      <div>
+         Cart
+      </div>
+   )
+}
+
+export default CartScreen
+
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,13 +2,22 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
+import { cartReducer } from './reducers/cartReducers'
 
 const reducer = combineReducers({
    productList: productListReducer,
    productDetails: productDetailsReducer,
+   cart: cartReducer,
 })
+// if cartItems found in loclastorage, then create cartItemsFromStorage variable, else it will be empty array
+const cartItemsFromStorage = localStorage.getItem('cartItems')
+   ? JSON.parse(localStorage.getItem('cartItems'))
+   : []
 
-const initialState = {}
+const initialState = {
+   cart: { cartItems: cartItemsFromStorage }
+
+}
 
 const middleware = [thunk]
 const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
