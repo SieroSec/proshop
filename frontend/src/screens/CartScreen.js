6f701670fa40da40f9812d54a31c3042d05db741
SieroSec <SieroSec@gmail.com>
2020-12-29 14:08:54 +0100

    Section 6: Add To Cart Functionality

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,14 +1,28 @@
-// import React, { useEffect } from 'react'
-// import { useDispatch, useSelector } from 'react-redux'
-// import { Row, Col } from 'react-bootstrap'
-// import Product from '../components/Product'
-// import Message from '../components/Message'
-// import Loader from '../components/Loader'
-// import { listProducts, listsProducts } from '../actions/productActions'
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
+import Message from '../components/Message'
+import { addToCart } from '../actions/cartActions'
 
-import React from 'react'
+const CartScreen = ({ match, location, history }) => {
+   const productId = match.params.id
 
-const CartScreen = () => {
+   // location.search === ?qty=1
+   const qty = location.search ? Number(location.search.split('=')[1]) : 1
+
+   const dispatch = useDispatch()
+
+   const cart = useSelector(state => state.cart)
+   const { cartItems } = cart
+
+   useEffect(() => {
+      if (productId) {
+         dispatch(addToCart(productId, qty))
+      }
+
+   }, [dispatch, productId, qty])
+
    return (
       <div>
          Cart
