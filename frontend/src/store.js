38e6d005270c0774bd67538e37b2253d41e11fca
SieroSec <SieroSec@gmail.com>
2020-12-31 15:07:57 +0100

    Section 8: 43. User Login Reducer & Action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,26 +1,32 @@
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
+import { userLoginReducer } from './reducers/userReducers'
 
 const reducer = combineReducers({
    productList: productListReducer,
    productDetails: productDetailsReducer,
    cart: cartReducer,
+   userLogin: userLoginReducer,
 })
 // if cartItems found in loclastorage, then create cartItemsFromStorage variable, else it will be empty array
 const cartItemsFromStorage = localStorage.getItem('cartItems')
    ? JSON.parse(localStorage.getItem('cartItems'))
    : []
 
-const initialState = {
-   cart: { cartItems: cartItemsFromStorage }
+// get userInfo from browser storage
+const userInfoFromStorage = localStorage.getItem('userInfo')
+   ? JSON.parse(localStorage.getItem('userInfo'))
+   : null
 
+const initialState = {
+   cart: { cartItems: cartItemsFromStorage },
+   userLogin: { userInfo: userInfoFromStorage },
 }
 
 const middleware = [thunk]
 const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
 
 export default store
-
